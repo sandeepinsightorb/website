f27aea06a538fd32e881fe641b627d73ffae009c
sandeepchintala <sandeep.chintala@insightorb.com>
2020-05-08 19:22:37 +0530

    added links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,9 +11,9 @@ import { ContactusComponent } from './contactus/contactus.component';
 import { SinglepostComponent } from './singlepost/singlepost.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent},
   { path: 'ourwork', component: OurworkComponent },
   { path: 'our-work-2', component: OurworktwoComponent },
   { path: 'services', component: OurservicesComponent },
